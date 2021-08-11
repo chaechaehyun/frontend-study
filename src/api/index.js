df7c3f4c71edb68b2f1a923c9d5d46019b823d89
hyeon-chae <chaehyun282@naver.com>
2021-08-11 11:33:23 +0900

    login api 통신 수정중

--- src/api/index.js
+++ src/api/index.js
@@ -2,23 +2,21 @@ import axios from "axios";
 // import store from '../store/store'
 
 const config = {
-  baseUrl: 'https://api.lastorder.co.kr/v1'
-  // baseApiUrl: 'http://127.0.0.1:8000/v1'
-  // baseApiUrl: 'http://13.124.153.201/v1'
-  // baseApiUrl: 'http://52.79.208.191/v1'
+  baseUrl: 'https://api-dev.lastorder.io/v1'
+  // baseUrl: 'https://api.lastorder.co.kr/v1'
 };
 
 // function fetchAccessToken() {
 //   return axios.post(`${config.baseUrl}/auth/token/`);
 // }
-function fetchAccessToken(data) {
-  return axios.post(`${config.baseUrl}/auth/token/${data}`);
-}
+// function fetchAccessToken(formData) {
+//   return axios.post(`${config.baseUrl}/auth/token/${formData}`);
+// }
 
 function fetchGoodsList() {
-  return axios.get(`${config.baseUrl}/shop/items/summary/`)
+  return axios.get(`${config.baseUrl}/shop/items`)
 }
 export {
-  fetchAccessToken,
+  // fetchAccessToken,
   fetchGoodsList,
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,39 +1,68 @@
 import { fetchAccessToken, fetchGoodsList } from "../api/index";
+// const baseApiUrl = 'https://api.lastorder.co.kr/v1';
+const baseApiUrl = 'api-dev.lastorder.io/v1';
+
+import axios from 'axios'
 
 const queryString = require("query-string");
 const clientId = "LrRxKVve7SZ5jiW41cUrnYgr9aFEfBy2IKHZPvSJ";
 const clientSecret =
   "k6aX4zw3UkFhMNW9X4TaoHWHmcvrhpW35EJLSpK7fzDZsgfPUDmF6LASEEN2zMXk2nuCSTTLYUgOH3369QcnDv8lUsJ93jeIcBBSu7qg8FTJbJ1y4ybaFPaiInLXadZq";
+
 // const clientId = 'aSNRAXNR10ZiiEfjLhjelA1h4kPMPV2cAwOONA1E';
-// const clientSecret = 'm5r1d6oQHe5J4AWGfolymgp1NmVxKRKELbFQAtSTqf12cT8kUPyvaZJzsdhvorQkQnYj8z6OrY4s8Mu3FZMGVIpsrw3WtzOeQ3nfFs53Sz3DjRfPoVCS2x4SlazvF1qM';
+// const clientSecret =
+//     'm5r1d6oQHe5J4AWGfolymgp1NmVxKRKELbFQAtSTqf12cT8kUPyvaZJzsdhvorQkQnYj8z6OrY4s8Mu3FZMGVIpsrw3WtzOeQ3nfFs53Sz3DjRfPoVCS2x4SlazvF1qM';
+
 const scope = 'SHOP';
-const myHeaders = new Headers({
-    'Content-Type': 'application/x-www-form-urlencoded'
-});
 
 export default {
 
-    async FETCH_ACCESS_TOKEN({ commit }, { id, password }) {
-    // console.log(id, password);
-        let formData = queryString.stringify({
-            'grant_type': 'password',
-            'client_id': clientId,
-            'client_secret': clientSecret,
-            'username': id,
-            'password': password,
-            'scope': scope,
-        })
-        console.log(formData);
-        const response = await fetchAccessToken(formData);
-        commit("SET_ACCESS_TOKEN", response.data.access_token);
-        return response;
-    },
+     FETCH_ACCESS_TOKEN( {commit}, {id, password} ) {
+		commit('SET_ACCESS_TOKEN', '');
+		delete axios.defaults.headers.common["Authorization"];
+		let formData = {
+			'grant_type': 'password',
+			'client_id': clientId,
+			'client_secret': clientSecret,
+			'username': id,
+			'password': password,
+			'scope': scope
+		}
+		return axios({				
+				method: 'post',
+				url: `${baseApiUrl}/auth/token/`,
+				data: queryString.stringify(formData),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			})
+			.then(response => {	
+				commit('SET_ACCESS_TOKEN', response.data.access_token);
+				
+				return response;
+				
+			})			
+    },    
+
+    // async FETCH_ACCESS_TOKEN({ commit }, { id, password }) {
+    // // console.log(id, password);
+    //     let formData = queryString.stringify({
+    //         'grant_type': 'password',
+    //         'client_id': clientId,
+    //         'client_secret': clientSecret,
+    //         'username': id,
+    //         'password': password,
+    //         'scope': scope,
+    //     })
+    //     console.log(formData);
+    //     const response = await fetchAccessToken(formData);
+    //     commit("SET_ACCESS_TOKEN", response.data.access_token);
+    //     return response;
+    // },
     
     async FETCH_GOODS_LIST({ commit }){
         const response = await fetchGoodsList();
         commit("SET_GOODS_LIST", response.data);
+        console.log(response.data);
         return response;
-        
     }
 
 };
